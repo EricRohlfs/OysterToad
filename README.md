7d437139e9a69d7d23b1cdf296414d0258ff8878
Eric Rohlfs <ericghent@gmail.com>
2018-08-11 17:41:23 -0400

    setting up project

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # OysterToad
 A drop-in solution for upgrading ASP.NET Webforms to Vue.js
+
+Moving from server side Webforms for client side JavaScript has never been easier.
+
+
+## Usage
+
+_1. Add .html to the end of your .aspx file.
+
+    rename page_name.aspx to page_name.aspx.html
+
+Add Vue and Oyster links to the bottom of your page.
+
+_2. Add links to the bottom of the .aspx.html page.
+
+```
+ <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js" type="text/javascript"></script>
+ <script src="oystertoad.js" type="text/javascript"></script>
+```
---
+++ src/oystertoad.js
@@ -0,0 +1,45 @@
+/*!
+* OysterToad JavaScript Library v 0.1
+* https://github.com/EricRohlfs/OysterToad
+* 
+* Copyright Eric Rohlfs
+* Released under the MIT license
+*
+* Date: 2018-08-11
+*/
+
+Vue.component('asp-image', {
+    props: ['imageurl', 'id', 'cssclass'],
+    template: '<img v-bind:id="id" v-bind:src="imageurl" v-bind:class="cssclass"/>'
+});
+
+Vue.component('asp-label', {
+    props: ['text', 'cssclass', 'associatedcontrolid'],
+    template: '<label v-bind:class="cssclass" v-bind:for="associatedcontrolid">{{text}}</label>'
+});
+
+Vue.component('asp-textbox', {
+    props: ['cssclass','text'],
+    template: '<input type="text" v-bind:class="cssclass" v-bind:value="text"></input>'
+});
+
+Vue.component('asp-button', {
+    props: ['text', 'cssclass','onclick'],
+    template: '<button type="button" v-bind:class="cssclass" v-bind:onclick="onclick">{{text}}</button>'
+});
+
+(function () {
+    //delete the code behind at the top of the page.
+    codeBehind = document.querySelector("body").childNodes[0];
+    codeBehind.parentNode.removeChild(codeBehind)
+
+    //convert asp:tagName to asp-tagname
+    formId = document.getElementsByTagNameNS("http://www.w3.org/1999/xhtml", "asp:form")[0].getAttribute('id');
+    newContent = document.querySelector('#' + formId).innerHTML.replace(/asp:/gi, 'asp-');
+    var form2 = Vue.compile('<div>'+ newContent + '</div>');
+    new Vue({
+        el: '#' + formId,
+        render: form2.render,
+        staticRenderFns: form2.staticRenderFns
+    });
+})();
